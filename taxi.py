1dc6a320f5923d9216b4c3661ca81cc99bdd865a
fraserdscott <fraserdscott@gmail.com>
2019-02-27 14:21:27 +0000

    Added some methods

--- taxi.py
+++ taxi.py
@@ -2,3 +2,12 @@ class Taxi:
     location = (0,0)
     ride = None
 
+    def drive():
+        # This function will be responsible for moving a taxi towards
+        # either the start or end destination of its current ride,
+        # waiting until its current ride is ready,
+        # or nothing if it doesnt have a ride
+
+    def move(destination):
+        # this function will changed the location of the taxi to
+        # move it towards a given coordinate destination
