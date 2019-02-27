b5ef3845f76d0a54f999457a317f0940de8b0aaa
veselypeta <veselypeta@gmail.com>
2019-02-27 14:13:35 +0000

    finished parse

--- taxis.py
+++ taxis.py
@@ -6,8 +6,8 @@ def parse_data(filename):
         lines = [x.strip('\n') for x in data_file.readlines()]
         n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps = map(int, tuple(lines[0].split(' ')))
         theCity = City(n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps)
-        ridesList = [x for x in map(lines[1:]]
-
-        return theCity, rides
+        ridesList = [list(map(int, x)) for x in [y.split(" ") for y in lines[1:]]]
+        rideObjects = [Ride(*tuple(x)) for x in ridesList]
+        return theCity, rideObjects
 
 city, rides = parse_data("b_should_be_easy.in")
