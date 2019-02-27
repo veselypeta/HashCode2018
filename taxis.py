6911f0186bb697b8823ae1b482ae0a4f74faf33d
veselypeta <veselypeta@gmail.com>
2019-02-27 15:23:52 +0000

    add comments

--- taxis.py
+++ taxis.py
@@ -2,11 +2,16 @@ from Ride import Ride
 from City import City
 from taxi import Taxi
 
+# parse the data from the file
 def parse_data(filename):
     with open(filename, 'r') as data_file:
+        # readlines
         lines = [x.strip('\n') for x in data_file.readlines()]
+        # process the first line
         n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps = map(int, tuple(lines[0].split(' ')))
+        # create a city class to hold this data
         theCity = City(n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps)
+        # The remaining lines are rides - which are processed and added to a list of ride objects
         ridesList = [list(map(int, x)) for x in [y.split(" ") for y in lines[1:]]]
         ridesNumber = [list(x + [i]) for i,x in enumerate(ridesList)]
         rideObjects = [Ride(*tuple(x)) for x in ridesNumber]
