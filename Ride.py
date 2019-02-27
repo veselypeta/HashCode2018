0b34b6b8352df7186e1d999cef8f19baa138cc82
fraserdscott <fraserdscott@gmail.com>
2019-02-27 13:47:58 +0000

    Added Ride class and __init__.py file
    __init__ allows imports for other files

---
+++ Ride.py
@@ -0,0 +1,12 @@
+class Ride:
+    start = -1
+    end = -1
+    earliest = -1
+    latest = -1
+
+    def __init__(self,start,end,earliest,latest):
+        self.start = start
+        self.end = end
+        self.earliest = earliest
+        self.latest = latest
+
