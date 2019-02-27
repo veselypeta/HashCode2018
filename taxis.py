78224a81e2ad14f746ced17143979e982d26a79e
veselypeta <veselypeta@gmail.com>
2019-02-27 13:45:37 +0000

    init

--- taxis.py
+++ taxis.py
@@ -3,7 +3,7 @@
 def parse_data(filename):
     with open(filename, 'r') as data_file:
         lines = [x.strip('\n') for x in data_file.readlines()]
-        print(lines)
+        n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps = tuple(lines[0].split(' '))
 
 
-parse_data("a_example.in")
+parse_data("b_should_be_easy.in")
