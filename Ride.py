920c25845e2519ccccfe456068d7313fde039961
veselypeta <veselypeta@gmail.com>
2019-02-27 14:53:49 +0000

    finished parse fully

--- Ride.py
+++ Ride.py
@@ -1,11 +1,15 @@
 class Ride:
 
 
-    def __init__(self,start_row = -1, start_column = -1,end_row=-1, end_column = -1, earliest=-1,latest=-1):
+    def __init__(self,start_row = -1, start_column = -1,end_row=-1, end_column = -1, earliest=-1,latest=-1, id = -1):
         self.start = (start_row, start_column)
         self.finish = (end_row, end_column)
         self.earliest = earliest
         self.latest = latest
+        self.ride_number = id
+    
+    def __str__(self):
+        pass
 
 
     def calculate_distance(self):
--- taxis.py
+++ taxis.py
@@ -1,5 +1,6 @@
 from Ride import Ride
 from City import City
+from queue import *
 
 def parse_data(filename):
     with open(filename, 'r') as data_file:
@@ -7,7 +8,12 @@ def parse_data(filename):
         n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps = map(int, tuple(lines[0].split(' ')))
         theCity = City(n_rows, n_columns, n_vehicles, n_rides, ride_bonus, n_steps)
         ridesList = [list(map(int, x)) for x in [y.split(" ") for y in lines[1:]]]
-        rideObjects = [Ride(*tuple(x)) for x in ridesList]
+        rideParameters = [list(x + [i]) for i,x  in enumerate(ridesList)]
+        rideObjects = [Ride(*tuple(x)) for x in rideParameters]
         return theCity, rideObjects
 
 city, rides = parse_data("b_should_be_easy.in")
+
+
+# create ride queues and taxi queue
+# then figure out how to sort the queue
