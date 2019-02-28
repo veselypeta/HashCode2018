79b9c4568b73e955bdd4b653eba2b3fc236df418
Daniel-Tee <s1733921@ed.ac.uk>
2019-02-28 13:34:45 +0000

    Third commit

--- github_changes_test.py
+++ github_changes_test.py
@@ -1,16 +1,13 @@
 # this is a test of github change conflicts
 
-a = 6 - 1 # changed lines 3, 4 and 5
-b = 1 - 1
-c = 9 - 1
 d = 'd'
 e = 'e'
 
-myList = [a,b,c,d,e]
+f = 'f'
+
+myList = [d,e,f]
+
+# adding some more comments to get a conflict
 
 for i in range(0,len(myList)):
     print(myList[i])
-
-# changes
-
-f = 'f'
